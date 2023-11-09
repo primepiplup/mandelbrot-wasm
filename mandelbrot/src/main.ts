b507cd5a6105f947ee8f38a58799d168a84ff07c
Daan Kuik <dkuik@sogyo.nl>
2023-11-09 17:30:47 +0100

    Mandelbrot zoom using webassembly

--- mandelbrot/src/main.ts
+++ mandelbrot/src/main.ts
@@ -1,12 +1,12 @@
 import { memory } from "../../mandelbrot-wasm/pkg/mandelbrot_wasm_bg.wasm";
-import { Renderer } from "../../mandelbrot-wasm/pkg/mandelbrot_wasm";
+import { Canvas } from "../../mandelbrot-wasm/pkg/mandelbrot_wasm";
 
 const canvas = document.getElementById("plot-canvas");
 const width: number = 600;
 const height: number = 600;
 canvas.width = width;
 canvas.height = height;
-const renderer = Renderer.new(width, height);
+const renderer = Canvas.new(width, height, "mandelbrot");
 const canvas_address = renderer.get_canvas();
 const array = new Uint8ClampedArray(memory.buffer, canvas_address, width * height * 4);
 const image = new ImageData(
@@ -14,4 +14,15 @@ const image = new ImageData(
 )
 const ctx = canvas.getContext("2d");
 
+const renderLoop = () => {
+  renderer.tick();
+  renderer.render();
+  ctx.putImageData(image, 0, 0);
+  requestAnimationFrame(renderLoop)
+}
+
+renderer.render();
 ctx.putImageData(image, 0, 0);
+
+renderLoop();
+
