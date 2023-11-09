0f9a2f0de0cc5b6b35b503b5ddb8e59aa8df816d
Daan Kuik <dkuik@sogyo.nl>
2023-11-09 09:08:04 +0100

    Implemented some operations on imaginary and complex numbers

--- mandelbrot-wasm/src/complex.rs
+++ mandelbrot-wasm/src/complex.rs
@@ -1,6 +1,88 @@
 use crate::imaginary::Imaginary;
+use std::ops::{Add, Mul, Sub};
 
+#[derive(Debug, PartialEq)]
 struct Complex {
     real: f64,
     imaginary: Imaginary,
+}
+
+impl Complex {
+    pub fn new(real: f64, imaginary: f64) -> Complex {
+        Complex {
+            real,
+            imaginary: Imaginary::new(imaginary),
+        }
+    }
+}
+
+impl Add<Complex> for Complex {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        let real = self.real + other.real;
+        let imaginary = self.imaginary + other.imaginary;
+        Complex { real, imaginary }
+    }
+}
+
+impl Sub<Complex> for Complex {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        let real = self.real - other.real;
+        let imaginary = self.imaginary - other.imaginary;
+        Complex { real, imaginary }
+    }
+}
+
+impl Mul<Complex> for Complex {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        let first = self.real * other.real;
+        let inner = other.imaginary * self.real;
+        let outer = self.imaginary * other.real;
+        let last = self.imaginary * other.imaginary;
+
+        Complex {
+            real: first + last,
+            imaginary: inner + outer,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn complex_numbers_are_multiplied_correctly() {
+        let cn_1 = Complex::new(6.0, 3.0);
+        let cn_2 = Complex::new(2.0, 5.0);
+
+        let multiplied = cn_1 * cn_2;
+
+        assert_eq!(multiplied, Complex::new(-3.0, 36.0));
+    }
+
+    #[test]
+    fn complex_numbers_are_added_properly() {
+        let cn_1 = Complex::new(8.0, 7.0);
+        let cn_2 = Complex::new(4.0, 3.0);
+
+        let added = cn_1 + cn_2;
+
+        assert_eq!(added, Complex::new(12.0, 10.0));
+    }
+
+    #[test]
+    fn complex_numbers_are_subtracted_properly() {
+        let cn_1 = Complex::new(7.0, 9.0);
+        let cn_2 = Complex::new(3.0, 4.0);
+
+        let added = cn_1 - cn_2;
+
+        assert_eq!(added, Complex::new(4.0, 5.0));
+    }
 }
--- mandelbrot-wasm/src/imaginary.rs
+++ mandelbrot-wasm/src/imaginary.rs
@@ -1,3 +1,50 @@
+use std::ops::{Add, Mul, Sub};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Imaginary {
-    real: f64,
+    number: f64,
+}
+
+impl Imaginary {
+    pub fn new(number: f64) -> Imaginary {
+        Imaginary { number }
+    }
+}
+
+impl Add<Imaginary> for Imaginary {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Imaginary {
+            number: self.number + other.number,
+        }
+    }
+}
+
+impl Sub<Imaginary> for Imaginary {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Imaginary {
+            number: self.number - other.number,
+        }
+    }
+}
+
+impl Mul<Imaginary> for Imaginary {
+    type Output = f64;
+
+    fn mul(self, other: Self) -> f64 {
+        self.number * other.number * -1.0
+    }
+}
+
+impl Mul<f64> for Imaginary {
+    type Output = Imaginary;
+
+    fn mul(self, other: f64) -> Self {
+        Imaginary {
+            number: self.number * other,
+        }
+    }
 }
