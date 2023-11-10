88f0a0047a98ca6532eab316e85a4a4f3bdf6002
Daan Kuik <dkuik@sogyo.nl>
2023-11-10 11:05:17 +0100

    Implemented tick() for default to produce animation

--- mandelbrot-wasm/src/renderables/default.rs
+++ mandelbrot-wasm/src/renderables/default.rs
@@ -1,17 +1,25 @@
 use crate::renderables::renderable::Renderable;
 
-pub struct Default {}
+pub struct Default {
+    base_color: u32,
+    counter: u32,
+}
 
 impl Renderable for Default {
     fn render(&self, x: usize, y: usize) -> u32 {
-        (x ^ y) as u32 | 0xFF_00_00_00
+        u32::wrapping_add(self.counter, (x ^ y) as u32 | self.base_color)
     }
 
-    fn tick(&mut self) {}
+    fn tick(&mut self) {
+        self.counter = u32::wrapping_add(self.counter, 1);
+    }
 }
 
 impl Default {
     pub fn new(_width: usize, _height: usize) -> Default {
-        Default {}
+        Default {
+            base_color: 0xFF_00_00_00,
+            counter: 0,
+        }
     }
 }
